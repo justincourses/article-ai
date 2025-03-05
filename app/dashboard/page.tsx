9591cea954b764769109da304db15af15f39a67f
Justin Chen <interjc@live.com>
2025-03-05 11:59:29 +0900

    :art: refactor writer ui

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,21 +1,16 @@
 import { Navbar } from "@/components/global/navbar";
 import { Footer } from "@/components/global/footer";
 
-import { UserDetails } from "@/components/global/user-details";
-
-import { CodeSwitcher } from "@/components/global/code-switcher";
+import { Writer } from "@/components/writer";
 
 export default async function DashboardPage() {
   return (
     <>
       <main className="max-w-[75rem] w-full mx-auto">
-        <div className="grid grid-cols-[1fr_20.5rem] gap-10 pb-10">
+        <div className="grid gap-10 pb-10">
           <div>
             <Navbar />
-            <UserDetails />
-          </div>
-          <div className="pt-[3.5rem]">
-            <CodeSwitcher />
+            <Writer />
           </div>
         </div>
       </main>
---
+++ components/writer/index.tsx
@@ -0,0 +1,9 @@
+import { UserDetails } from "@/components/global/user-details";
+
+export function Writer() {
+  return (
+    <div>
+      <UserDetails />
+    </div>
+  );
+}
